c67432d0d6302802efc879b0227bc8688dea1e98
John Detjen <johndetjen@Johns-MacBook-Pro-2.local>
2018-07-16 15:51:50 -0500

    new project

--- codes>/ViewController.swift
+++ codes>/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cameraButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UITabBar.appearance().barTintColor = UIColor(red: 0.0/255.0, green: 116.0/255.0, blue: 141.0/255.0, alpha: 1.0)
+        
+        cameraButton.layer.cornerRadius = 0.5 * cameraButton.bounds.size.width
+        cameraButton.clipsToBounds = true
+        cameraButton.imageEdgeInsets = UIEdgeInsetsMake(25,25,25,25)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
